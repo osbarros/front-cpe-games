c290addad1141dcbd8fff628f1b1b5a5bae99bf1
Davi Dumont <davifonsecadumont@hotmail.com>
2022-05-11 17:43:12 -0300

    Html pronto

--- cpe-games/src/Pages/Login/Login.js
+++ cpe-games/src/Pages/Login/Login.js
@@ -1,7 +1,24 @@
 import React from "react";
 
 function Login() {
-    return <h1>Login</h1>;
+    return (
+    <div>
+        <Header/>
+        <div className="container-login">
+        <h1 className="texto"> A melhor plataforma</h1>
+        <h1 className="segundo_texto">De E-Sports do Brasil</h1>
+        </div>
+
+    <div className="container-login"> 
+        <h1 className="texto">ACESSE SUA CONTA</h1>
+        <input type="text" className="input" placeholder="USUÁRIO"></input>  
+        <input type="password" className="input" placeholder="SENHA"></input> 
+        <button value="Entrar"></button>
+        <a href="">Esqueci minha senha</a>
+    </div>
+
+    </div>
+    )
 }
 
 export default Login;
