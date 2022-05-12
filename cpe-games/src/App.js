17d8e100e0b14cb8e750a7c0941e7e29331d6a3a
Gabriel Peroni <gabrielcperoni@gmail.com>
2022-05-11 23:19:18 -0300

    Main atualizada

--- cpe-games/src/App.js
+++ cpe-games/src/App.js
@@ -1,11 +1,11 @@
 import React from "react"
 import Rotas from "./routes";
 import "./Global.css"
-import Header from "./Components/Header/Header";
+//import Header from "./Components/Header/Header";
 //import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return <Header/>;
+  return <Rotas/>;
 }
 
 export default App;
