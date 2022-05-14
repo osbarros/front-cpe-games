a5d4d45ab92dde60e2539e64e704f9d5593ef2dc
osbarros <ceobarros@outlook.com>
2022-05-14 02:53:38 -0300

    Criação da Home Logada

---
+++ cpe-games/src/Components/HeaderLog/HeaderLog.js
@@ -0,0 +1,57 @@
+import React, {useState} from "react"
+import "./HeaderLog.css"
+
+
+function HeaderLog() {
+
+    const [active, setActive] = useState("header_alt_menu");
+        const mobile_menu = () => {
+            active === "header_alt_menu"
+              ? setActive("header_alt_menu alt_menu_active")
+              : setActive("header_alt_menu");
+        }; 
+    
+    return (
+        <div className="header_global">
+            <div onClick={mobile_menu} className = "mobile_menu">
+                <div className="line1"></div>
+                <div className="line2"></div>
+                <div className="line3"></div>
+            </div>
+            <div className = "header_esquerda">
+                <img src="/images/logo_cpe_preto.png" className="logo_cpe" alt="CPE"/>
+                <button className = "header_jogos">
+                    JOGOS 
+                </button>
+                <button className="header_campeonatos">
+                    CAMPEONATOS
+                </button>
+                <button className="header_loja">
+                    LOJA
+                </button>
+            </div>
+            <img src="/images/logo_cpe_preto.png" className="logo_cpe_mobile" alt="CPE"/>
+                <div className="header_direita">
+                    <div className="header_search" action="">
+                        <img src="/images/searchbar.svg" className="header_logo_searchbar" alt="searchbar"/>
+                        <input className="header_search_text" placeholder="Pesquisar..."></input>
+                    </div>
+                    <div className="sair_perfil">
+                        <img src="/images/logo_perfil.svg" className="logo_perfil" alt="perfil"/>
+                        <img src="/images/logo_sair.svg" className="logo_sair" alt="sair"/>
+                    </div>
+                </div>
+                <div className={active}>
+                    <button className="header_alt_jogos">JOGOS</button>
+                    <button className="header_alt_campeonatos">CAMPEONATOS</button>
+                    <button className="header_alt_loja">LOJA</button>
+                    <button className="header_alt_login">PERFIL</button>
+                    <button className="header_alt_inscrever">SAIR</button>
+                </div>
+                <img src= "/images/searchbar_mobile.png" className="search_mobile"></img>
+            </div>
+
+    )  
+}
+
+export default HeaderLog
--- cpe-games/src/Pages/Home/Home.js
+++ cpe-games/src/Pages/Home/Home.js
@@ -2,11 +2,12 @@ import React from "react";
 import Header from "./../../Components/Header";
 import HomeBody from "./../../Components/HomeBody";
 import Footer from "./../../Components/Footer";
+import HeaderLog from "../../Components/HeaderLog";
 
 function Home() {
     return (
         <div>
-            <Header/>
+            <HeaderLog/>
             <HomeBody/>
             <Footer/>
         </div>
