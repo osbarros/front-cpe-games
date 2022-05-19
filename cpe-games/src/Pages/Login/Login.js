99d95205929462b4c013dee5bdf7a12881ba712a
Davi Dumont <davifonsecadumont@hotmail.com>
2022-05-18 21:51:44 -0300

    Pagina Login

--- cpe-games/src/Pages/Login/Login.js
+++ cpe-games/src/Pages/Login/Login.js
@@ -1,21 +1,27 @@
 import React from "react";
-
+import Header from "./../../Components/Header";
+import "./Login.css";
 function Login() {
     return (
-    <div>
+    <div className="login_base">
         <Header/>
-        <div className="container-login">
-        <h1 className="texto"> A melhor plataforma</h1>
+        <div className="login_page"> 
+        <div className="container_title">
+        <h1 className="primeiro_texto"> A melhor plataforma</h1>
         <h1 className="segundo_texto">De E-Sports do Brasil</h1>
         </div>
 
-    <div className="container-login"> 
-        <h1 className="texto">ACESSE SUA CONTA</h1>
+        <div className="container_login"> 
+        <h1 className="texto_a">ACESSE SUA CONTA</h1>
+        <h2 className="USUÁRIO">USUÁRIO</h2>
         <input type="text" className="input" placeholder="USUÁRIO"></input>  
+        <h2 className="SENHA">SENHA</h2>
         <input type="password" className="input" placeholder="SENHA"></input> 
-        <button value="Entrar"></button>
+        <button value="Entrar">ENTRAR</button>
         <a href="">Esqueci minha senha</a>
     </div>
+    </div>
+        
 
     </div>
     )
