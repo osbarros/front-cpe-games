c225c8303abf280b2610d8f60b8f887bda8cc726
samuel rebula <rebuuula@gmail.com>
2022-05-20 23:23:58 -0300

    adicionado footer na pagina de cadastro e correção bug bootstrap

--- cpe-games/src/Pages/Cadastro/Cadastro.js
+++ cpe-games/src/Pages/Cadastro/Cadastro.js
@@ -1,12 +1,14 @@
 import React from "react";
 import CadastroBody from "../../Components/CadastroBody";
 import Header from "../../Components/Header";
+import Footer from "../../Components/Footer"
 
 function Cadastro() {
     return (
         <div>
         <Header/>
         <CadastroBody/>
+        <Footer/>
         </div>
     );
 }
