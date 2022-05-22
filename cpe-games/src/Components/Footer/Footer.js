e2d2a85d0d1aab58396dcf166e8b47a9f2307c77
Hugo Marinho <hugomarinho@cpejr.com.br>
2022-05-21 21:49:40 -0300

    Botões crescem "on hover" + eliminação de código desnecessário
    No footer, os textos foram convertidos para botões para acomodar essa mudança (na verdade, eu já deveria ter colocado isso lá). Fiz algumas eliminações no CSS no Header pra tirar um bug que ocorreria ao repousar o mouse sobre o botão de login.

--- cpe-games/src/Components/Footer/Footer.js
+++ cpe-games/src/Components/Footer/Footer.js
@@ -8,9 +8,9 @@ function Footer() {
         <div className="footer-container">
             <img src="/images/logo_cpe_preto.png" className="footer-img-logo" alt="CPE"></img>
             <div className="footer-text-container">
-                <p className="footer-text">Trabalhe Conosco</p>
-                <p className="footer-text">Fale com a gente</p>
-                <p className="footer-text">Faq</p>
+                <button className="footer-text">Trabalhe Conosco</button>
+                <button className="footer-text">Fale com a gente</button>
+                <button className="footer-text">Faq</button>
             </div>
             <div className="footer-social-container">
                 <img className="footer-img-social" src="/images/linkedin.png" alt="LinkedIn"></img>
