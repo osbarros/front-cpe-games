87a5f86f2869d88aec69d105e2072bd10bbf770c
Gabriel Peroni <gabrielcperoni@gmail.com>
2022-05-26 13:07:51 -0300

    link front-back login

--- cpe-games/src/Components/LoginBody/LoginBody.js
+++ cpe-games/src/Components/LoginBody/LoginBody.js
@@ -1,6 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import "./LoginBody.css";
+import {useHistory} from "react-router-dom";
+import api from "../../services/api";
+
+
 function LoginBody() {
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const history = useHistory();
+
+    async function login(e) {
+        e.preventDefault();
+        try {
+            const response = await api.post('/login', {email, password});
+            alert("Bem vindo", response.data.user.nome_usuario);
+            history.push("/home");
+            console.log(response);
+        } catch (error) {
+            if(error.response.status === 403){
+                alert("Credenciais inválidas!");
+            }
+            else{
+                alert(error.response.data.notification);
+            }
+            console.warn(error);
+        }
+    }
+
+
+
     return (
         <div className="login_base">
             
@@ -12,10 +40,12 @@ function LoginBody() {
 
                 <div className="container_login">
                     <h1 className="texto_a_Login">ACESSE SUA CONTA</h1>
-                    <h2 className="usuario_Login">USUÁRIO</h2>
-                    <input type="text" className="input" placeholder="USUÁRIO"></input>
+                    <h2 className="usuario_Login">E-MAIL</h2>
+                    <input type="text" className="input" placeholder="E-MAIL"
+                    onChange={(e) => setEmail(e.target.value)}></input>
                     <h2 className="senha_Login">SENHA</h2>
-                    <input type="password" className="input" placeholder="SENHA"></input>
+                    <input type="password" className="input" placeholder="SENHA"
+                        onChange={(e) => setPassword(e.target.value)}></input>
                     <button value="Entrar">ENTRAR</button>
                     <a href="">Esqueci minha senha</a>
                 </div>
